71ec97c6d9ac4b9cd769cc471b58e4f51a167b9f
stefandaius <stefan.iordache2001@gmail.com>
2021-03-20 17:56:13 +0200

    canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import CanvasDraw from 'react-canvas-draw'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    let savableCanvas = null;
+
+    let canvas = "";
+
+    const style = {
+        button: {
+            width: 100,
+            height: 50,
+            margin: 20
+        }
+    }
+
+    return (
+        <div className="App">
+            <CanvasDraw ref={(canvas) => {
+                savableCanvas = canvas;
+            }} canvasWidth={1000} canvasHeight={700} brushRadius={3} lazyRadius={0} saveData={canvas}/>
+            <button style={style.button} onClick={() => savableCanvas.undo()}>UNDO</button>
+            <button style={style.button} onClick={() => savableCanvas.clear()}>CLEAR</button>
+            <button style={style.button} onClick={() => console.log(savableCanvas.getSaveData())}>SAVE</button>
+        </div>
+    );
+};
 
 export default App;
